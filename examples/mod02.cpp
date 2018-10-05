c3d1c9bfb57c65d97880e6ecce5fb7b1edcb52fc
Neel Basu <neel.basu.z@gmail.com>
2018-10-05 20:10:29 +0530

    - in progress

--- examples/mod02.cpp
+++ examples/mod02.cpp
@@ -3,6 +3,7 @@
 #include <mathematica++/transport.h>
 #include <mathematica++/defs.h>
 
+#include <boost/noncopyable.hpp>
 #include <boost/tuple/tuple.hpp>
 #include <boost/function.hpp>
 #include <boost/fusion/container.hpp>
@@ -12,6 +13,10 @@
 #include <boost/fusion/functional/invocation/invoke.hpp>
 
 using namespace mathematica;
+
+MATHEMATICA_DECLARE(Complex);
+
+namespace mathematica{
 
 namespace internal{
     template <typename T>
@@ -30,6 +35,22 @@ namespace internal{
         typedef boost::tuple<Args...>           tuple_type;
         typedef boost::fusion::tuple<Args...>   arguments_type;
     };
+    
+
+    template<std::size_t...Is, class T>
+    auto to_fusion( std::index_sequence<Is...>, T&& in ) {
+        using boost::get;
+        return boost::fusion::make_tuple( get<Is>(std::forward<T>(in))... );
+    }
+    template<class...Ts>
+    boost::fusion::tuple<Ts...> to_fusion( boost::tuple<Ts...> in ) {
+        return to_fusion( std::make_index_sequence<sizeof...(Ts)>{}, std::move(in) );
+    }
+    template<class...Ts>
+    boost::fusion::tuple<Ts...> to_fusion( std::tuple<Ts...> in ) {
+        return to_fusion( std::make_index_sequence<sizeof...(Ts)>{}, std::move(in) );
+    }
+    
 }
 
 template <typename Function>
@@ -44,6 +65,7 @@ struct module_overload{
     std::vector<std::string> _heads;
     
     module_overload(function_type f): _function(f){}
+    module_overload(const self_type& other): _function(other._function), _heads(other._heads){}
     
     self_type& operator=(std::initializer_list<std::string> heads){
         std::copy(heads.begin(), heads.end(), std::back_inserter(_heads));
@@ -54,7 +76,9 @@ struct module_overload{
      * check number of arguments supplied on runtime and number of arguments with which this overload has been prepared at compile time.
      */
     bool feasible(boost::shared_ptr<mathematica::tokens::function> args) const{
+        std::clog << "feasible " << __LINE__ << std::endl;
         if(_heads.empty()){
+            std::clog << "feasible " << (boost::fusion::result_of::size<arguments_type>::type::value  == args->nargs()) << __LINE__ << std::endl;
             return boost::fusion::result_of::size<arguments_type>::type::value  == args->nargs();
         }else{
             if(_heads.size() != args->nargs()) return false;
@@ -62,9 +86,11 @@ struct module_overload{
             for(mathematica::value v: args->_args){
                 boost::shared_ptr<mathematica::tokens::function> f = boost::dynamic_pointer_cast<mathematica::tokens::function>(v);
                 if(!f || f->name() != *it++){
+                    std::clog << "feasible " << false << __LINE__ << std::endl;
                     return false;
                 }
             }
+            std::clog << "feasible " << true << __LINE__ << std::endl;
             return true;
         }
     }
@@ -77,8 +103,10 @@ struct module_overload{
         return call(args);
     }
     return_type call(boost::shared_ptr<mathematica::tokens::function> args){
-        arguments_type arguments = cast<arguments_type>(args);
-        // https://www.boost.org/doc/libs/1_68_0/libs/fusion/doc/html/fusion/functional/invocation/functions/invoke.html
+        tuple_type tuple = cast<tuple_type>(args);
+        arguments_type arguments = internal::to_fusion(tuple);
+//         std::clog << arguments << std::endl;
+//         // https://www.boost.org/doc/libs/1_68_0/libs/fusion/doc/html/fusion/functional/invocation/functions/invoke.html
         return boost::fusion::invoke(_function, arguments);
     }
     return_type operator()(boost::shared_ptr<mathematica::tokens::function> args){
@@ -100,22 +128,40 @@ module_overload<F> overload(F ftor){
     return module_overload<F>(ftor);
 }
 
-struct resolver{
+struct resolver: private boost::noncopyable{
     mathematica::wtransport& _shell;
-    bool _returned;
+    bool _resolved;
     
-    resolver(mathematica::wtransport& shell): _shell(shell), _returned(false){}
+    resolver(mathematica::wtransport& shell): _shell(shell), _resolved(false){}
+    bool resolved() const{return _resolved;}
+};
+
+namespace library{
+    struct error: public std::exception{
+        int _err;
+        
+        explicit error(int err): _err(err){}
+        virtual const char* what() const noexcept{
+            return (boost::format("mathematica::library::error: internal library implementation returned (%1%)") % _err).str().c_str();
+        }
+        int code() const{return _err;}
+    };
 };
 
 template <typename T>
-resolver& operator,(resolver& r, T& overload){
-    if(!r._returned){
+mathematica::resolver& operator,(mathematica::resolver& r, T overload){
+    std::clog << "operator, " << __LINE__ << std::endl;
+    if(!r._resolved){
+        std::clog << "operator, " << __LINE__ << std::endl;
         if(overload.feasible(r._shell.input())){           // args is a shared pointer to a function token boost::shared_ptr<mathematica::tokens::function>
+            std::clog << "operator, " << __LINE__ << std::endl;
             overload.out(r._shell.input(), r._shell);      // redirects the call to callback function and writes the returned output to shell
-            r._returned = true;
+            r._resolved = true;
         }
     }
     return r;
+}
+
 }
 
 template <typename T>
@@ -123,6 +169,12 @@ struct point_2d{
     T x;
     T y;
 };
+
+template <typename T>
+std::ostream& operator<<(std::ostream& stream, const point_2d<T>& pt){
+    stream << "(" << pt.x << " " << pt.y << ")"; 
+    return stream;
+}
 
 namespace mathematica{
     template <typename T>
@@ -133,20 +185,22 @@ namespace mathematica{
 }
 
 int some_function_impl0(point_2d<double> p1, point_2d<double> p2){
-    return 0;
+    std::clog << "some_function_impl0 " << __LINE__ << std::endl;
+    return p1.x+p2.y;
 }
 int some_function_impl1(double x, double y){
-    return 1;
+    std::clog << "some_function_impl1 " << __LINE__ << std::endl;
+    return 10;
 }
-int some_function_impl2(double x){
-    return 2;
+mathematica::m some_function_impl2(double x){
+    std::clog << "some_function_impl2 " << __LINE__ << std::endl;
+    return List(x+2, x);
 }
-
 
 EXTERN_C DLLEXPORT mint WolframLibrary_getVersion(){
     return WolframLibraryVersion;
 }
-EXTERN_C DLLEXPORT int WolframLibrary_initialize(WolframLibraryData libData){
+EXTERN_C DLLEXPORT mint WolframLibrary_initialize(WolframLibraryData libData){
     return 0;
 }
 EXTERN_C DLLEXPORT void WolframLibrary_uninitialize(WolframLibraryData libData){
@@ -154,35 +208,50 @@ EXTERN_C DLLEXPORT void WolframLibrary_uninitialize(WolframLibraryData libData){
 }
 
 EXTERN_C DLLEXPORT int SomeFunctionX(WolframLibraryData libData, WMK_LINK native_link){
-    mathematica::wtransport shell(libData, native_link);
-    resolver resolve(shell);
+    std::ofstream out("/tmp/log");
+    std::clog.rdbuf(out.rdbuf());
     
-    resolve , overload(&some_function_impl0) = {"GeoPosition", "GeoPosition"}
-            , overload(&some_function_impl1)
-            , overload(&some_function_impl2);
-
-    return LIBRARY_TYPE_ERROR;
-}
-
-
-// MATHEMATICA_MODULE_BEGIN(IsAcuteTriangle)
-//     MATHEMATICA_MODULE_RELATE("GeoPosition", point_2d<double>)
-//     MATHEMATICA_MODULE_OVERLOAD(is_acute_impl, "GeoPosition", "GeoPosition", "GeoPosition")
-//     MATHEMATICA_MODULE_OVERLOAD(is_acute_impl, point_2d, point_2d, point_2d)
-//     MATHEMATICA_MODULE_OVERLOAD(is_acute_impl, double, double, double, double, double, double)
-// MATHEMATICA_MODULE_END()
-// 
-// 
-// EXTERN_C DLLEXPORT int IsAcuteTriangle(WolframLibraryData libData, WMK_LINK native_link){
-//     mathematica::wtransport shell(libData, native_link);
-//     mathematica::value input = shell.input();
-//     boost::shared_ptr<mathematica::tokens::function> list = boost::dynamic_pointer_cast<mathematica::tokens::function>(input);
-//     int _nargs = list->nargs();
-// 
-//     if(_nargs == 3){
-//         
+    mathematica::wtransport shell(libData, native_link);
+    mathematica::resolver resolver(shell);
+    
+  
+//     {
+//         auto resolver = overload(&some_function_impl0) = {"GeoPosition", "GeoPosition"};
+//         if(resolver.feasible(shell.input())){                                        // args is a shared pointer to a function token boost::shared_ptr<mathematica::tokens::function>
+//             resolver.out(shell.input(), shell);                                      // redirects the call to callback function and writes the returned output to shell
+//             return LIBRARY_NO_ERROR;
+//         }
 //     }
-// }
+//     
+//     {
+//         auto resolver = overload(&some_function_impl1);
+//         if(resolver.feasible(shell.input())){                                        // args is a shared pointer to a function token boost::shared_ptr<mathematica::tokens::function>
+//             resolver.out(shell.input(), shell);                                      // redirects the call to callback function and writes the returned output to shell
+//             return LIBRARY_NO_ERROR;
+//         }
+//     }
+// 
+//     {
+//         auto resolver = overload(&some_function_impl2);
+//         if(resolver.feasible(shell.input())){                                        // args is a shared pointer to a function token boost::shared_ptr<mathematica::tokens::function>
+//             resolver.out(shell.input(), shell);                                      // redirects the call to callback function and writes the returned output to shell
+//             return LIBRARY_NO_ERROR;
+//         }
+//     }
+    
+    try{
+        
+        resolver, overload(&some_function_impl0) = {"GeoPosition", "GeoPosition"}
+                , overload(&some_function_impl1)
+                , overload(&some_function_impl2);
+                
+    }catch(const mathematica::library::error& err){
+        std::clog << "error " << __LINE__ << std::endl;
+        return err.code();
+    }
+    
+    return LIBRARY_NO_ERROR;
+}
 
 
 // SomeFunctionX = LibraryFunctionLoad["/home/neel/Projects/mathematicapp/build/examples/libmod02.so", "SomeFunctionX", LinkObject, LinkObject]
